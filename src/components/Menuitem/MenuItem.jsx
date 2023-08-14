75fa5c81bc7d25a87d80c91ad576b4eedae4cda9
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-14 19:30:11 +0100

    started styling the menu component

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+const MenuItem = ({ title, price, tags }) => (
+	<div className="app__menuItems">
+		<div className="app__menuItems-head">
+			<div className="app__menuItems-name">
+				<p className="p__cormorant" style={{ color: "DCCA87" }}>
+					{title}
+				</p>
+			</div>
+
+			<div className="app__menuItems-name">
+				<p className="p__cormorant">{price}</p>
+			</div>
+		</div>
+	</div>
 );
 
 export default MenuItem;
