3cfc8b12dd4fe541a9f5b686e54197374e0adcfb
TemitopeOgunrekun <olalekanogunrekun@gmail.com>
2023-08-19 16:01:55 +0100

    finished styling the footer

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -5,7 +5,6 @@ import "./FooterOverlay.css";
 const FooterOverlay = () => (
 	<div className="app__footerOverlay">
 		<div className="app__footerOverlay-black"></div>
-
 		<div className="app__footerOverlay-img app__bg"></div>
 	</div>
 );
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import SubHeading from "../SubHeading/SubHeading";
 import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
 
 const Newsletter = () => (
 	<div className="app__newsletter">
 		<div className="app__newsletter-heading">
 			<SubHeading title="Newsletter" />
+			<h1 className="headtext__cormorant">Suscribe To Our Newsletter</h1>
+			<p className="p__opensans">And never miss latest Updates!</p>
+		</div>
+		<div className="app__newsletter-input flex__center">
+			<input type="email" placeholder="Enter your email address" />
+			<button type="button" className="custom__button">
+				Suscribe
+			</button>
 		</div>
 	</div>
 );
